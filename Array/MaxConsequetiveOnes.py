326ee8305f3cf1b7d596cfad7fa24b7651e104cf
Sarveshwari kumari <indiansarveshwarii@gmail.com>
2026-01-21 23:00:26 +0530

    changeAgain

--- Array/MaxConsequetiveOnes.py
+++ Array/MaxConsequetiveOnes.py
@@ -13,3 +13,6 @@ class Solution:
         return maxCount
 
 
+# This function finds the maximum number of consecutive 1s in the array.
+# It counts continuous 1s and resets the count when 0 is found.
+# The maximum count is updated during traversal.
